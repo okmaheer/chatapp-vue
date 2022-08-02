4304438af6436deead8b5641295ae83e20fb9bcc
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-02 16:37:27 +0500

    changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import LoginView from './components/LoginView.vue';
+import ChatView from './components/ChatView.vue';
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.component('LoginView', LoginView);
+app.component('ChatView', ChatView);
+
+app.mount('#app');
