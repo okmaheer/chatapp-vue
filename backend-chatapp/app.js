2bb0055676e43d98690d39c524ad67e3cce54cbc
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-03 13:08:24 +0500

    finish app without db

--- backend-chatapp/app.js
+++ backend-chatapp/app.js
@@ -16,6 +16,7 @@ const io = require("socket.io")(http, {
 let users = [];
 let messages = [];
 let index = 0;
+let currentUser = "";
 
 
 io.on("connection", socket =>{
@@ -27,14 +28,16 @@ socket.emit("loggedIn",{
 socket.on('newuser', username =>{
   console.log(`${username} has entered the Chat Group`);
   socket.username = username;
+  currentUser = username;
   users.push(socket);
   io.emit('userOnline',socket.username);
 });
-socket.on('msg',msg =>{
+socket.on('msg',data =>{
+  console.log(currentUser);
   let message = {
     index:index,
-    username:socket.username,
-    msg:msg,
+    username:data.username,
+    msg:data.msg,
   }
   messages.push(message);
   io.emit('msg',message);
