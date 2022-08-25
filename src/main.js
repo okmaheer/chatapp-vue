53258bf60487c244ce2889f4de5b4c0ba436bfad
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-25 17:42:47 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import LoginView from './components/LoginView.vue';
 import ChatView from './components/ChatView.vue';
 const app = createApp(App);
 
+
 app.component('LoginView', LoginView);
 app.component('ChatView', ChatView);
 
